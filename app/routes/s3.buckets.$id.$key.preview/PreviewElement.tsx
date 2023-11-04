2a1f5e1a87890a730bf470ed2ccea61d3e7cd499
Giorgio Garasto <giorgio@garasto.me>
2023-11-04 11:39:19 +0100

    feat: implement very basic json preview

--- app/routes/s3.buckets.$id.$key.preview/PreviewElement.tsx
+++ app/routes/s3.buckets.$id.$key.preview/PreviewElement.tsx
@@ -1,6 +1,14 @@
 import { DataGrid, DataGridProps } from '@mui/x-data-grid';
-import { FunctionComponent, useState } from 'react';
-import { unstable_useEnhancedEffect as useEnhancedEffect } from '@mui/material';
+import { TreeItem, TreeView, TreeViewProps } from '@mui/x-tree-view';
+import { FunctionComponent, ReactNode, useState } from 'react';
+import {
+  styled,
+  unstable_useEnhancedEffect as useEnhancedEffect,
+} from '@mui/material';
+import {
+  ExpandMore as ExpandMoreIcon,
+  ChevronRight as ChevronRightIcon,
+} from '@mui/icons-material';
 
 export interface PreviewElementProps {
   contentType: string;
@@ -8,9 +16,9 @@ export interface PreviewElementProps {
   src: string;
 }
 
-const CsvViewer: FunctionComponent<Pick<PreviewElementProps, 'src'>> = ({
-  src,
-}) => {
+const CsvViewer: FunctionComponent<
+  Pick<PreviewElementProps, 'name' | 'src'>
+> = ({ name, src }) => {
   const [dataGridProps, setDataGridProps] = useState<DataGridProps | undefined>(
     undefined,
   );
@@ -35,7 +43,102 @@ const CsvViewer: FunctionComponent<Pick<PreviewElementProps, 'src'>> = ({
     );
   }, []);
 
-  return dataGridProps && <DataGrid {...dataGridProps} />;
+  return dataGridProps && <DataGrid aria-label={name} {...dataGridProps} />;
+};
+
+const ColoredKey = styled('strong')(({ theme }) => ({
+  color: theme.palette.primary.main,
+}));
+const ColoredValue = styled('span')<{ $value: any }>(({ $value, theme }) => ({
+  ...(typeof $value === 'number' && {
+    color: theme.palette.secondary.dark,
+  }),
+  ...(typeof $value === 'string' && {
+    color: theme.palette.success.dark,
+  }),
+  ...(typeof $value === 'boolean' && {
+    color: theme.palette.info.dark,
+  }),
+  ...($value === null && {
+    color: theme.palette.error.dark,
+  }),
+}));
+const JsonValue: FunctionComponent<{ value: any }> = ({ value }) => (
+  <ColoredValue $value={value}>{JSON.stringify(value)}</ColoredValue>
+);
+const JsonTreeView = styled(TreeView)(({ theme }) => ({
+  background: theme.palette.background.default,
+  width: '100%',
+  height: '100%',
+  overflow: 'auto',
+})) as typeof TreeView;
+
+const convertToTree = (obj: any, keyPrefix = ''): ReactNode => {
+  if (Array.isArray(obj) || (typeof obj === 'object' && obj !== null)) {
+    return Object.entries(obj).map(([key, value]) => {
+      if (
+        Array.isArray(value) ||
+        (typeof value === 'object' && value !== null)
+      ) {
+        const nodeId = `${keyPrefix}['${key}']`;
+        return (
+          <TreeItem
+            key={nodeId}
+            nodeId={nodeId}
+            label={
+              <code>
+                <ColoredKey>{key}:</ColoredKey>
+              </code>
+            }
+          >
+            {convertToTree(value, nodeId)}
+          </TreeItem>
+        );
+      }
+      const nodeId = `${keyPrefix}['${key}']`;
+      return (
+        <TreeItem
+          key={nodeId}
+          nodeId={nodeId}
+          label={
+            <code>
+              <ColoredKey>{key}:</ColoredKey> <JsonValue value={value} />
+            </code>
+          }
+        />
+      );
+    });
+  }
+  return <JsonValue key={`${keyPrefix}['${obj}']`} value={obj} />;
+};
+
+const JsonViewer: FunctionComponent<
+  Pick<PreviewElementProps, 'name' | 'src'>
+> = ({ name, src }) => {
+  const [treeViewProps, setTreeViewProps] = useState<
+    TreeViewProps<false> | undefined
+  >(undefined);
+
+  useEnhancedEffect(() => {
+    fetch(src)
+      .then(response => response.json())
+      .then(data => {
+        // Convert the JSON object to a tree structure.
+        const children = convertToTree(data);
+        setTreeViewProps({ defaultExpanded: Object.keys(data), children });
+      });
+  }, []);
+
+  return (
+    treeViewProps && (
+      <JsonTreeView
+        aria-label={name}
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+        {...treeViewProps}
+      />
+    )
+  );
 };
 
 export const PreviewElement: FunctionComponent<PreviewElementProps> = ({
@@ -53,7 +156,10 @@ export const PreviewElement: FunctionComponent<PreviewElementProps> = ({
     return <audio src={src} title={name} controls />;
   }
   if (contentType.startsWith('text/csv')) {
-    return <CsvViewer src={src} />;
+    return <CsvViewer src={src} name={name} />;
+  }
+  if (contentType.startsWith('application/json')) {
+    return <JsonViewer src={src} name={name} />;
   }
   return <iframe src={src} title={name} />;
 };
