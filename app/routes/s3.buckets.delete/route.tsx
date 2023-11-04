09982a60bebda93d2e16caf02c5ce273c647746f
Giorgio Garasto <giorgio@garasto.me>
2023-11-04 10:47:49 +0100

    fix: empty buckets before trying to delete them

--- app/routes/s3.buckets.delete/route.tsx
+++ app/routes/s3.buckets.delete/route.tsx
@@ -1,6 +1,6 @@
 import { DeleteBucketCommand, S3Client } from '@aws-sdk/client-s3';
 import { ActionFunctionArgs, redirect } from '@remix-run/node';
-import { setupAwsClients } from '~/src/aws/server';
+import { emptyBucket, setupAwsClients } from '~/src/aws/server';
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
@@ -8,13 +8,16 @@ export async function action({ request }: ActionFunctionArgs) {
   const bucketsToDelete = formData.get('ids')?.toString().split(',') ?? [];
 
   await Promise.all(
-    bucketsToDelete.map(bucket =>
-      s3Client.send(
+    bucketsToDelete.map(async bucket => {
+      // Empty the bucket first
+      await emptyBucket(s3Client, bucket);
+      // Then delete it
+      await s3Client.send(
         new DeleteBucketCommand({
           Bucket: bucket,
         }),
-      ),
-    ),
+      );
+    }),
   );
 
   return redirect('/s3/buckets');
