4c6ea72c5791cae4d2a46c2030c4ece862093db1
Giorgio Garasto <giorgio@garasto.me>
2023-11-05 08:12:07 +0100

    fix: table overlay ref issue

--- app/src/components/TableOverlay.tsx
+++ app/src/components/TableOverlay.tsx
@@ -1,25 +1,24 @@
-import { FunctionComponent, PropsWithChildren } from 'react';
+import { FunctionComponent, PropsWithChildren, forwardRef } from 'react';
 import { Stack, StackProps, SvgIconProps } from '@mui/material';
 
 export interface TableOverlayProps extends StackProps {
   icon?: FunctionComponent<SvgIconProps>;
 }
 
-const TableOverlay: FunctionComponent<PropsWithChildren<TableOverlayProps>> = ({
-  icon: Icon,
-  children,
-  ...props
-}) => (
-  <Stack
-    width="100%"
-    height="100%"
-    alignItems="center"
-    justifyContent="center"
-    {...props}
-  >
-    {Icon && <Icon sx={{ width: '4rem', height: '4rem', mb: 1 }} />}
-    {children}
-  </Stack>
+const TableOverlay = forwardRef<HTMLDivElement, TableOverlayProps>(
+  ({ icon: Icon, children, ...props }, ref) => (
+    <Stack
+      ref={ref}
+      width="100%"
+      height="100%"
+      alignItems="center"
+      justifyContent="center"
+      {...props}
+    >
+      {Icon && <Icon sx={{ width: '4rem', height: '4rem', mb: 1 }} />}
+      {children}
+    </Stack>
+  ),
 );
 
 export default TableOverlay;
