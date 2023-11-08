23c6f8ae3c02a7871ee3b869fe7945b0a9154258
Giorgio Garasto <giorgio@garasto.me>
2023-11-08 19:16:57 +0100

    feat: add support for creating dlqs

--- app/routes/sqs.queues.($name)/CreateQueueDialog.tsx
+++ app/routes/sqs.queues.($name)/CreateQueueDialog.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent, useState } from 'react';
 import {
+  Autocomplete,
   Button,
   Checkbox,
   FormControlLabel,
@@ -7,21 +8,44 @@ import {
   Stack,
   TextField,
 } from '@mui/material';
+import { SerializeFrom } from '@remix-run/server-runtime';
 import ConfirmationDialog from '~/src/components/ConfirmationDialog';
 import useLinkUtils from '~/src/hooks/useLinkUtils';
+import { loader } from './route';
 
 export interface CreateQueueDialogProps {
   open: boolean;
+  existingQueues: SerializeFrom<typeof loader>['queues'];
 }
 
 const fifoSuffix = '.fifo';
 
 const CreateQueueDialog: FunctionComponent<CreateQueueDialogProps> = ({
   open,
+  existingQueues,
 }) => {
   const { withSearchParam } = useLinkUtils();
   const [isFifo, setIsFifo] = useState(false);
+  const [providedDlqName, setProvidedDlqName] = useState('');
+  const dlqName = `${providedDlqName}${isFifo ? fifoSuffix : ''}`;
   const maxNameLength = isFifo ? 80 - fifoSuffix.length : 80;
+  const availableDlqs = isFifo
+    ? existingQueues
+        .filter(queue => queue.QueueName.endsWith(fifoSuffix))
+        .map(queue => queue.QueueName.slice(0, -fifoSuffix.length))
+    : existingQueues
+        .filter(queue => !queue.QueueName.endsWith(fifoSuffix))
+        .map(queue => queue.QueueName);
+  const dlqExists = availableDlqs.includes(providedDlqName);
+
+  const computeHelperText = () => {
+    if (!providedDlqName) {
+      return 'No DLQ will be created';
+    }
+    return dlqExists
+      ? `The queue "${dlqName}" will be used as DLQ`
+      : `A queue named "${dlqName}" will be created and used as DLQ`;
+  };
 
   return (
     <ConfirmationDialog
@@ -50,7 +74,7 @@ const CreateQueueDialog: FunctionComponent<CreateQueueDialogProps> = ({
               },
             })}
           />
-          <Stack mt={2}>
+          <Stack mt={1}>
             <FormControlLabel
               control={
                 <Checkbox
@@ -58,13 +82,59 @@ const CreateQueueDialog: FunctionComponent<CreateQueueDialogProps> = ({
                   onChange={event => setIsFifo(event.target.checked)}
                 />
               }
-              label="FIFO"
+              label="First-in-first-out"
             />
             <FormControlLabel
               control={<Checkbox name="contentBasedDeduplication" />}
               label="Content-based deduplication"
             />
+            <FormControlLabel
+              control={<Checkbox name="sqsManagedSseEnabled" />}
+              label="SQS-managed server-side encryption"
+            />
           </Stack>
+          <Autocomplete
+            freeSolo
+            disableClearable
+            options={availableDlqs}
+            renderInput={params => (
+              <TextField
+                {...params}
+                label="Dead-letter queue"
+                name="dlqName"
+                helperText={computeHelperText()}
+                inputProps={{
+                  ...params.inputProps,
+                  // Queues can only contain alphanumeric characters, hyphens, or underscores. 1 to 80 in length
+                  pattern: `^[\\w\\-]{1,${maxNameLength}}$`,
+                  minLength: 1,
+                  maxLength: maxNameLength,
+                }}
+                {...(isFifo && {
+                  InputProps: {
+                    ...params.InputProps,
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        {fifoSuffix}
+                      </InputAdornment>
+                    ),
+                  },
+                })}
+              />
+            )}
+            inputValue={providedDlqName}
+            onInputChange={(_, newValue) => setProvidedDlqName(newValue)}
+            sx={{ mt: 1, mb: 1.5 }}
+          />
+          <TextField
+            fullWidth
+            label="Max receive count"
+            name="maxReceiveCount"
+            type="number"
+            disabled={!providedDlqName}
+            inputProps={{ min: 1, max: 1000 }}
+            sx={{ mb: 2 }}
+          />
         </>
       }
       closeLink={withSearchParam('create', null)}
--- app/routes/sqs.queues.($name)/actions.ts
+++ app/routes/sqs.queues.($name)/actions.ts
@@ -2,23 +2,94 @@ import {
   CreateQueueCommand,
   DeleteQueueCommand,
   SendMessageCommand,
+  GetQueueAttributesCommand,
+  GetQueueUrlCommand,
+  QueueAttributeName,
+  SQSClient,
 } from '@aws-sdk/client-sqs';
 import { ActionFunctionArgs, redirect } from '@remix-run/node';
 import { getAwsClient } from '~/src/aws/server';
+
+const getOrCreateQueueUrl = async (
+  sqsClient: SQSClient,
+  name: string,
+  attributes?: Partial<Record<QueueAttributeName, string>>,
+): Promise<string | undefined> => {
+  const existingQueue = await sqsClient
+    .send(
+      new GetQueueUrlCommand({
+        QueueName: name,
+      }),
+    )
+    .catch(() => ({ QueueUrl: undefined }));
+
+  if (existingQueue.QueueUrl) {
+    return existingQueue.QueueUrl;
+  }
+
+  const createdQueueOutput = await sqsClient.send(
+    new CreateQueueCommand({
+      QueueName: name,
+      Attributes: attributes,
+    }),
+  );
+
+  return createdQueueOutput.QueueUrl;
+};
+
+const getOrCreateQueueArn = async (
+  sqsClient: SQSClient,
+  name: string,
+  attributes?: Partial<Record<QueueAttributeName, string>>,
+): Promise<string | undefined> => {
+  const queueUrl = await getOrCreateQueueUrl(sqsClient, name, attributes);
+  const queueAttributes = await sqsClient.send(
+    new GetQueueAttributesCommand({
+      QueueUrl: queueUrl,
+      AttributeNames: ['QueueArn'],
+    }),
+  );
+
+  return queueAttributes.Attributes?.QueueArn;
+};
 
 export const createQueueAction = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData();
   const sqsClient = getAwsClient('sqs');
   const isFifo = formData.has('fifo');
+  const FifoQueue = isFifo.toString();
+  const ContentBasedDeduplication = formData
+    .has('contentBasedDeduplication')
+    .toString();
+  const SqsManagedSseEnabled = formData.has('sqsManagedSseEnabled').toString();
+  const commonAttributes: Partial<Record<QueueAttributeName, string>> = {
+    FifoQueue,
+    ContentBasedDeduplication,
+    SqsManagedSseEnabled,
+  };
+
+  // If a DLQ name is provided, create the DLQ first (or get its URL if it already exists)
+  const providedDlqName = formData.get('dlqName')?.toString();
+  const dlqName = `${providedDlqName}${isFifo ? '.fifo' : ''}`;
+  const dlqArn = providedDlqName
+    ? await getOrCreateQueueArn(sqsClient, dlqName, commonAttributes)
+    : '';
 
   await sqsClient.send(
     new CreateQueueCommand({
       QueueName: `${formData.get('name')?.toString()}${isFifo ? '.fifo' : ''}`,
       Attributes: {
-        FifoQueue: isFifo.toString(),
-        ContentBasedDeduplication: formData
-          .has('contentBasedDeduplication')
-          .toString(),
+        FifoQueue,
+        ContentBasedDeduplication,
+        SqsManagedSseEnabled,
+        ...(dlqArn
+          ? {
+              RedrivePolicy: JSON.stringify({
+                deadLetterTargetArn: dlqArn,
+                maxReceiveCount: formData.get('maxReceiveCount')?.toString(),
+              }),
+            }
+          : {}),
       },
     }),
   );
