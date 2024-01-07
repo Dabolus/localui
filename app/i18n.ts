712ab6c018f1a64ce009b5400a724a336cb9b718
Giorgio Garasto <giorgio@garasto.me>
2024-01-07 08:03:32 +0100

    chore: count empty translations as not translated

--- app/i18n.ts
+++ app/i18n.ts
@@ -6,6 +6,8 @@ export default {
   // This is the language you want to use in case
   // if the user language is not in the supportedLngs
   fallbackLng: 'en',
+  // Count empty translations as not translated
+  returnEmptyString: false,
   // The default namespace of i18next is "translation", but you can customize it here
   defaultNS: 'common',
   // Disabling suspense is recommended
