c3b779405fc5741386dc1986bc6142c8b89b2bc0
Giorgio Garasto <giorgio@garasto.me>
2024-01-07 08:18:20 +0100

    chore: tweak i18n settings

--- app/i18n.ts
+++ app/i18n.ts
@@ -6,8 +6,14 @@ export default {
   // This is the language you want to use in case
   // if the user language is not in the supportedLngs
   fallbackLng: 'en',
+  // Count null translations as not translated
+  returnNull: false,
   // Count empty translations as not translated
   returnEmptyString: false,
+  // React already escapes values
+  interpolation: {
+    escapeValue: false,
+  },
   // The default namespace of i18next is "translation", but you can customize it here
   defaultNS: 'common',
   // Disabling suspense is recommended
