6bedb1aac5c4d82c9202cab717da84be677063e5
Giorgio Garasto <giorgio@garasto.me>
2024-01-20 07:58:22 +0100

    chore: enable source maps

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [remix(), tsconfigPaths()],
   build: {
+    sourcemap: true,
     rollupOptions: {
       onwarn(warning, warn) {
         // Ignore warnings related to "use client" directives
