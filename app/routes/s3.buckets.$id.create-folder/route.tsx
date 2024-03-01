5f499ef6b9e60c533de165f297d8c6fd8caf62de
Giorgio Garasto <giorgio@garasto.me>
2024-03-01 13:23:01 +0100

    fix: folder creation leading/trailing slashes + redirect

--- app/routes/s3.buckets.$id.create-folder/route.tsx
+++ app/routes/s3.buckets.$id.create-folder/route.tsx
@@ -7,20 +7,21 @@ export async function action({ request, params }: ActionFunctionArgs) {
   const { searchParams } = new URL(request.url);
   const formData = await request.formData();
   const s3Client = getAwsClient('s3', searchParams.get('endpoint'));
-  const providedName = formData.get('name')?.toString();
-  const fullName = providedName?.endsWith('/')
-    ? providedName
-    : `${providedName ?? ''}/`;
-  const prefix = searchParams.get('prefix') ?? '';
+  const providedName = formData
+    .get('name')
+    ?.toString()
+    .trim()
+    // Remove leading and trailing slashes
+    .replace(/^\/+|\/+$/g, '');
+  const prefix = searchParams.get('prefix');
+  const fullName = `${prefix ?? ''}${providedName ?? ''}/`;
 
   await s3Client.send(
     new PutObjectCommand({
       Bucket: params.id,
-      Key: `${searchParams.get('prefix') ?? ''}${fullName}`,
+      Key: fullName,
     }),
   );
 
-  return redirect(
-    `/s3/buckets/${params.id}${prefix ? `/${base64UrlEncode(prefix)}` : ''}`,
-  );
+  return redirect(`/s3/buckets/${params.id}/${base64UrlEncode(fullName)}`);
 }
